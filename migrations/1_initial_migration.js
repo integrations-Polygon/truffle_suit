858e34c6e6b169504d09309545db5821a63eedaa
Hukam Singh <imhukam18@gmail.com>
2022-12-09 18:46:44 +0530

    added auto fetch gas api

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,15 @@
 const Migrations = artifacts.require("MyToken");
+const fetch = require("node-fetch");
+let maxGas;
+
+async function fetch_gas() {
+  const gas_resp = await fetch('https://gasstation-mumbai.matic.today/v2')
+  const gas = await gas_resp.json();
+  maxGas = parseInt((gas.fast.maxFee) * 1e9);
+  console.log(maxGas)
+}
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations,{gasPrice: 30000000000,chainId: 80001});
+  fetch_gas();
+  deployer.deploy(Migrations, { gasPrice: maxGas, chainId: 80001 });
 };
